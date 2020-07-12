b253bf57db8d8efa04473096c643393df47386be
Javier Martinez <javi@64robots.com>
2020-07-12 23:32:05 +0200

    spacing fixes

--- src/extension.js
+++ src/extension.js
@@ -1,38 +1,38 @@
-const vscode = require('vscode');
+const vscode = require('vscode')
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
+  const provider1 = vscode.languages.registerCompletionItemProvider('vue', {
+    provideCompletionItems(document, position, token, context) {
+      // Getter list
+      // TODO: Read them from store files
+      const getter1 = new vscode.CompletionItem(
+        "'projects/activeProject'",
+        vscode.CompletionItemKind.Text
+      )
+      getter1.insertText = "activeProject: 'projects/activeProject'"
 
-	const provider1 = vscode.languages.registerCompletionItemProvider('vue', {
+      // Snippets
+      const snippetCompletion = new vscode.CompletionItem('...mapGetters')
+      snippetCompletion.kind = vscode.CompletionItemKind.Keyword
+      snippetCompletion.insertText = new vscode.SnippetString('...mapGetters({\r\t${1}\r}),')
+      snippetCompletion.documentation = new vscode.MarkdownString('Inserts a mapGetters block')
+      // TODO: Show only suggestions about getters
+      snippetCompletion.command = {
+        command: 'editor.action.triggerSuggest',
+        title: 'Re-trigger completions...',
+      }
 
-		provideCompletionItems(document, position, token, context) {
+      // return all completion items as array
+      return [getter1, snippetCompletion]
+    },
+  })
 
-            // Getter list
-            // TODO: Read them from store files
-            const getter1 = new vscode.CompletionItem('\'projects/activeProject\'', vscode.CompletionItemKind.Text)
-			getter1.insertText = 'activeProject: \'projects/activeProject\'';
-
-            // Snippets
-            const snippetCompletion = new vscode.CompletionItem('...mapGetters');
-			snippetCompletion.kind = vscode.CompletionItemKind.Keyword;
-			snippetCompletion.insertText = new vscode.SnippetString('...mapGetters({\r\t${1}\r}),');
-            snippetCompletion.documentation = new vscode.MarkdownString("Inserts a mapGetters block");
-            // TODO: Show only suggestions about getters
-			snippetCompletion.command = { command: 'editor.action.triggerSuggest', title: 'Re-trigger completions...' };
-
-			// return all completion items as array
-			return [
-                getter1,
-				snippetCompletion,
-			];
-		}
-	});
-
-	context.subscriptions.push(provider1);
+  context.subscriptions.push(provider1)
 }
 
-exports.activate = activate;
+exports.activate = activate
 
 module.exports = { activate }
